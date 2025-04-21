09f93298bb3e9249d3ec889c2552c706d1081259
Anishkhan15 <ka2955159@gmail.com>
2025-04-21 23:18:08 +0530

    Updated project files

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,19 +1,18 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Import from 'react-dom/client' instead of 'react-dom'
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HelmetProvider } from 'react-helmet-async';  // ✅ Add this line
 
-// Create a root element using `createRoot`
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the App component
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>  {/* ✅ Wrap App inside HelmetProvider */}
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
-// Optional: For measuring performance
 reportWebVitals();
